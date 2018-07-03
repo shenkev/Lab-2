1450376797b3d5f5bd24df2986e0edeeec8a164b
shenkev <tingkekevin@hotmail.com>
2018-07-02 20:30:29 -0400

    made Problem version of files

--- Problem/dataset.py
+++ Problem/dataset.py
@@ -5,14 +5,18 @@ class AdultDataset(data.Dataset):
 
     def __init__(self, X, y):
 
-        self.X = X
-        self.y = y
+        ######
+
+        # 3.1 YOUR CODE HERE
+
+        ######
 
     def __len__(self):
         return len(self.X)
 
     def __getitem__(self, index):
-        categorical = self.X[index]
-        label =self.y[index]
+        ######
 
-        return categorical, label
+        # 3.1 YOUR CODE HERE
+
+        ######
--- Problem/model.py
+++ Problem/model.py
@@ -7,17 +7,16 @@ class MultiLayerPerceptron(nn.Module):
 
         super(MultiLayerPerceptron, self).__init__()
 
-        self.input_size = input_size
-        self.output_size = 1
+        ######
 
-        self.fc1 = nn.Linear(self.input_size, 64)
-        self.nonlin1 = nn.Tanh()
-        self.fc2 = nn.Linear(64, 1)
-        self.out = nn.Sigmoid()
+        # 3.3 YOUR CODE HERE
+
+        ######
 
     def forward(self, features):
 
-        x = self.nonlin1(self.fc1(features))
-        x = self.fc2(x)
+        ######
 
-        return self.out(x)
+        # 3.3 YOUR CODE HERE
+
+        ######
